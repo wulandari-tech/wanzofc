cf8780b1774b93e50d72c9dcc23849f6a8ddc349
wanzofc08 <wanzofc08@gmail.com>
2025-03-01 02:26:23 +0700

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,15 +13,15 @@ function Main() {
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/wanzbrayy" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://github.com/wanzbrayy" target="_blank" rel="noreferrer"><github/></a>
           </div>
-          <h1>Yuji Sato</h1>
+          <h1>WANZOFC</h1>
           <p>Full Stack Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/wanzbrayy" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://github.com/wanzbrayy" target="_blank" rel="noreferrer"><GITHUB/></a>
           </div>
         </div>
       </div>
